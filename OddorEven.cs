c64eb22867e98b50b6d9e6840a4fce4d470dfee7
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-07-21 22:47:46 +0530

    Update OddorEven.cs

--- OddorEven.cs
+++ OddorEven.cs
@@ -1,22 +1,22 @@
-﻿//using System;
+﻿using System;
 
-//namespace PROJECT
-//{
-//    internal class OddorEven
-//    {
-//        public static void main(string[] args)
-//        {
-//            Console.WriteLine("Enter a number:");
-//            int number = Convert.ToInt32(Console.ReadLine());
+namespace PROJECT
+{
+    internal class OddorEven
+    {
+        public static void main(string[] args)
+        {
+            Console.WriteLine("Enter a number:");
+            int number = Convert.ToInt32(Console.ReadLine());
 
-//            if (number % 2 == 0)
-//            {
-//                Console.WriteLine("Even");
-//            }
-//            else
-//            {
-//                Console.WriteLine("Odd");
-//            }
-//        }
-//    }
-//}
+            if (number % 2 == 0)
+            {
+                Console.WriteLine("Even");
+            }
+            else
+            {
+                Console.WriteLine("Odd");
+            }
+        }
+    }
+}
