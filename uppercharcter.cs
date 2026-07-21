316f27c42603253f3b334caaed5a60a7efefc9d5
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-07-21 22:49:48 +0530

    Restore uppercharcter.cs with original functionality

--- uppercharcter.cs
+++ uppercharcter.cs
@@ -1,22 +1,21 @@
-﻿////Write a program to convert given name in upper characters.
-////INPUT : John F Kennedy
-////OUTPUT: JOHN F KENNEDY
-//using System;
+﻿//Write a program to convert given name in upper characters.
+//INPUT : John F Kennedy
+//OUTPUT: JOHN F KENNEDY
+using System;
 
-//namespace ConsoleApplication1
-//{
-//    internal class uppercharcter
-//    {
-//        public static void Main(string[] args)
-//        {
-//            Console.Write("Enter Name: ");
-//            string name = Console.ReadLine() ?? "";
+namespace ConsoleApplication1
+{
+    internal class uppercharcter
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine() ?? "";
 
-//            string upperName = name.ToUpper();
+            string upperName = name.ToUpper();
+            Console.WriteLine("Output: " + upperName);
 
-//            Console.WriteLine("Output: " + upperName);
-
-//            Console.ReadLine();
-//        }
-//    }
-//}
+            Console.ReadLine();
+        }
+    }
+}
