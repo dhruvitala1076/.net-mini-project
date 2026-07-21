de0be13e649331704bfc98a0ce9b0d415cc715bf
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-07-21 22:48:51 +0530

    Restore toggle case program implementation

--- togglecase.cs
+++ togglecase.cs
@@ -1,29 +1,29 @@
-﻿////Write a Program to convert given name in toggle case.
-////INPUT : JoHn F kEnNedy
-////OUTPUT: jOhN f KeNneDY
+﻿//Write a Program to convert given name in toggle case.
+//INPUT : JoHn F kEnNedy
+//OUTPUT: jOhN f KeNneDY
 
-//using System;
+using System;
 
-//class togglecase
-//{
-//    public static void Main(string[] args)
-//    {
-//        Console.Write("Enter Name: ");
-//        string name = Console.ReadLine() ?? "";
+class togglecase
+{
+    public static void Main(string[] args)
+    {
+        Console.Write("Enter Name: ");
+        string name = Console.ReadLine() ?? "";
 
-//        string result = "";
+        string result = "";
 
-//        foreach (char ch in name)
-//        {
-//            if (char.IsUpper(ch))
-//                result += char.ToLower(ch);
-//            else if (char.IsLower(ch))
-//                result += char.ToUpper(ch);
-//            else
-//                result += ch;
-//        }
+        foreach (char ch in name)
+        {
+            if (char.IsUpper(ch))
+                result += char.ToLower(ch);
+            else if (char.IsLower(ch))
+                result += char.ToUpper(ch);
+            else
+                result += ch;
+        }
 
-//        Console.WriteLine("Output: " + result);
-//        Console.ReadLine();
-//    }
-//}
+        Console.WriteLine("Output: " + result);
+        Console.ReadLine();
+    }
+}
