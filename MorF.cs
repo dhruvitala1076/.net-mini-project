886cd1b307051084a1fdfb18dafc37c0c072836c
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-07-22 22:13:36 +0530

    Update MorF.cs

--- MorF.cs
+++ MorF.cs
@@ -1,26 +1,26 @@
-﻿//Write a Program which accepts name and gender from the user. Here, gender may have only 1 character, M or F.
+//Write a Program which accepts name and gender from the user. Here, gender may have only 1 character, M or F.
 //Based on the gender prefix the name Mr. & Ms.
 //NAME : Hillary Clinton
 //GENDER : F
 using System;
-
-class Program
+namespace PROJECT_01P
 {
-    static void Main()
+    class MorF
     {
-        Console.Write("Enter Name: ");
-        string name = Console.ReadLine();
+        public static void M_F()
+        {
+            Console.Write("Enter Name: ");
+            string name = Console.ReadLine();
 
-        Console.Write("Enter Gender (M/F): ");
-        char gender = Convert.ToChar(Console.ReadLine());
+            Console.Write("Enter Gender (M/F): ");
+            char gender = Convert.ToChar(Console.ReadLine());
 
-        if (gender == 'M' || gender == 'm')
-            Console.WriteLine("Mr. " + name);
-        else if (gender == 'F' || gender == 'f')
-            Console.WriteLine("Ms. " + name);
-        else
-            Console.WriteLine("Invalid Gender");
-
-        Console.ReadLine();
+            if (gender == 'M' || gender == 'm')
+                Console.WriteLine("Ms. " + name);
+            else if (gender == 'F' || gender == 'f')
+                Console.WriteLine("Mr. " + name);
+            else
+                Console.WriteLine("Invalid Gender");
+        }
     }
 }
