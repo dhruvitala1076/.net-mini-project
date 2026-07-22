0356516b8ca87e608f12be9b97fe35cffe19c022
DHRUVI TALA <dhruvitala1007@gmail.com>
2026-07-22 22:06:47 +0530

    Implement pattern printing in Pattern5 class

--- pattern5.cs
+++ pattern5.cs
@@ -1,31 +1,33 @@
-﻿//using System;
+using System;
+namespace PROJECT_01P
+{
+    class Pattern5
+    {
+        public static void pat6()
+        {
+            for (int i = 1; i <= 5; i++)
+            {
+                for (int j = 5; j > i; j--)
+                    Console.Write(" ");
 
-//class Program
-//{
-//    static void Main()
-//    {
-//        for (int i = 1; i <= 5; i++)
-//        {
-//            for (int j = 5; j > i; j--)
-//                Console.Write(" ");
+                for (int k = 1; k <= (2 * i - 1); k++)
+                    Console.Write("*");
 
-//            for (int k = 1; k <= (2 * i - 1); k++)
-//                Console.Write("*");
+                Console.WriteLine();
+            }
 
-//            Console.WriteLine();
-//        }
+            for (int i = 4; i >= 1; i--)
+            {
+                for (int j = 5; j > i; j--)
+                    Console.Write(" ");
 
-//        for (int i = 4; i >= 1; i--)
-//        {
-//            for (int j = 5; j > i; j--)
-//                Console.Write(" ");
+                for (int k = 1; k <= (2 * i - 1); k++)
+                    Console.Write("*");
 
-//            for (int k = 1; k <= (2 * i - 1); k++)
-//                Console.Write("*");
+                Console.WriteLine();
+            }
 
-//            Console.WriteLine();
-//        }
-
-//        Console.ReadLine();
-//    }
-//}
+            Console.ReadLine();
+        }
+    }
+}
